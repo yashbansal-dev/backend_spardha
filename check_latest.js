0e713f6b8a7e84a4990b151f2ac73e02214760d4
yashbansal-dev <yashbansal531@gmail.com>
2026-03-18 01:08:13 +0530

    database

--- check_latest.js
+++ check_latest.js
@@ -8,7 +8,7 @@ async function checkLatest() {
         await mongoose.connect(mongoUri);
         
         console.log('--- LATEST 5 PURCHASES ---');
-        const purchases = await Purchase.find().sort({ _id: -1 }).limit(5);
+        const purchases = await Purchase.find().sort({ _id: -1 }).limit(20);
         for(let p of purchases) {
              console.log(`Order: ${p.orderId} | Status: ${p.paymentStatus} | Email: ${p.userDetails?.email} | userRegistered: ${p.userRegistered} | Date: ${p.purchaseDate}`);
              if (p.userDetails?.email) {
